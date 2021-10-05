e663a7db29fa67beecb2cae3684ff376c5e43b0c
Martin Metodiev <martin.metodiew@gmail.com>
2021-10-05 19:52:44 +0300

    refactor: change default app selector prefixes

--- apps/eshop/src/app/app.component.ts
+++ apps/eshop/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ng-eshop-root',
+  selector: 'eshop-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
