51d312fee19c0c813b16e7b77359907d950f2db0
Martin Metodiev <martin.metodiew@gmail.com>
2021-10-06 20:36:20 +0300

    refactor(admin-panel): clear up and adjust styles

--- apps/admin-panel/src/app/app.component.ts
+++ apps/admin-panel/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ng-eshop-root',
+  selector: 'admin-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'admin-panel';
