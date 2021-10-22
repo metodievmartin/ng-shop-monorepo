cd4bd424c38c2595f82483969c57926fcc47dee2
Martin Metodiev <martin.metodiew@gmail.com>
2021-10-22 17:59:10 +0300

    feat(eshop): create app footer

---
+++ apps/eshop/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'eshop-footer',
+  templateUrl: './footer.component.html',
+  styles: [
+  ],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class FooterComponent {
+
+  constructor() { }
+}
